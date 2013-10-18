bc3f41cfc13f88a8381249821449abd21a4a9cbc
Piermaria Cosina <piermaria@thedoersproject.com>
2013-10-18 15:20:17 +0200

    Automated commit at 2013-10-18 13:20:17 UTC by middleman-deploy 0.1.2

---
+++ javascripts/vendor/hidpi_modernizr_test.js
@@ -0,0 +1,1 @@
+Modernizr.addTest("highresdisplay",function(){if(window.matchMedia){var e=window.matchMedia("only screen and (-moz-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");return e.matches}});
